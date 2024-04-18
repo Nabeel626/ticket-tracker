b6ede958e51357a75063ce498ea46ee6d8276849
Nabeel <nabeel92914@outlook.com>
2024-04-18 10:25:18 +0100

    added a searchbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import './assets/App.scss';
 import EmployeeCardContainer from './EmployeeCard/EmployeeCardContainer';
-import SearchInputContainer from './SearchInput/SearchInputContainer';
-import data from './team';
+import { data } from './team';
 
 function App() {
 
   return (
     <>
       <h1 className='heading'>TICKET TRACKER</h1>
-      
-      <section className='search-employee'>
-        <SearchInputContainer users={data} />
-      </section>
 
       <section className='employee-cards'>
         <EmployeeCardContainer users={data} />
       </section>
 
     </>
-  )
+  ) 
 }
 
 export default App;
--- src/EmployeeCard/EmployeeCardContainer.tsx
+++ src/EmployeeCard/EmployeeCardContainer.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useState } from 'react';
+import SearchInput from '../SearchInput/SearchInput';
 import EmployeeCard from './EmployeeCard';
 
 type TeamData = {
@@ -9,13 +11,27 @@ type EmployeeData = {
     name: string;
     role: string; 
 };
-  
 
 const EmployeeCardContainer = ({users} : TeamData) => {
-  
+    
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    
+    const handleInput = (event : FormEvent<HTMLInputElement>) => {
+        const cleanedInput = event?.currentTarget.value.toLowerCase();
+
+        setSearchTerm(cleanedInput);
+    }
+
+    const filteredSearch = users.filter((user) => {                       
+        return user.name.toLowerCase().includes(searchTerm);
+        
+    });
+
     return (
-        <>
-            {users.map((user) => {
+        <>  
+            <SearchInput label="Search By Name" searchTerm={searchTerm} handleInput={handleInput} />
+
+            {filteredSearch.map((user) => {
                 return <EmployeeCard key={user.id} id={user.id} name={user.name} role={user.role} /> //This will map out the employee card so that it can return it
             })}
 
